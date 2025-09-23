534a89d942d3aa9b496a3ce1e75ec767c700d8a9
MichaelETU003327 <mikermb18@gmail.com>
2025-09-23 22:19:27 +0300

    Modification de FrontServlet.java
    Service est equivalent à doGet + doPost qui prend en charge les requetes et les reponses
    
    Tentative de recuperation du resultat de la méthode = valeur de retour de la fonction
    
    Si echec on affiche l'url

--- src/main/java/servlet/FrontServlet.java
+++ src/main/java/servlet/FrontServlet.java
@@ -33,6 +33,41 @@ public class FrontServlet extends HttpServlet {
         }
     }
 
-   
+    // service est equivalent à doGet + doPost qui prend en charge les requetes et
+    // les reponses
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        // pour recupérer la requete
+        String path = req.getPathInfo();
+
+        // pour avoir la méthode correspondante à cette requete (url)
+        Method action = routes.get(path);
+
+        // pour tester si la méthode existe ou non (ressource)
+        if (action != null) {
+            try {
+
+                // Tentative de recuperation du resultat de la méthode = valeur de retour de la
+                // fonction
+                Object controller = action.getDeclaringClass().getDeclaredConstructor().newInstance();
+                Object result = action.invoke(controller);
+
+                // On essaie d'afficher le résultat
+                resp.setContentType("text/plain");
+                resp.getWriter().print(result);
+
+            } catch (Exception e) {
+                throw new ServletException(e);
+            }
+        } else {
+
+            // Sinon on affiche l'url
+            resp.setContentType("text/plain");
+            resp.getWriter().print(path);
+
+        }
+    }
 
 }
