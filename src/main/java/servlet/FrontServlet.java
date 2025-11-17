930f5902b366f0cf9c73403792dbf234e1972ea3
MichaelETU003327 <mikermb18@gmail.com>
2025-11-17 05:23:43 +0300

    #4-bis
    On cree une classe ModelView {
    String view;
    }
    dans le framework
    
    puis on appelle dans le test , ModelView mv = new MV();
    mv.setView("test.jsp")

--- src/main/java/servlet/FrontServlet.java
+++ src/main/java/servlet/FrontServlet.java
@@ -2,6 +2,8 @@ package servlet;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -22,12 +24,41 @@ public class FrontServlet extends HttpServlet {
         String path = req.getRequestURI().substring(req.getContextPath().length());
         MethodInvoker invoker = (routes != null) ? routes.get(path) : null;
 
-        resp.setContentType("text/plain");
+
 
         if (invoker != null) {
             try {
-                String methodName = invoker.getMethod().getName();
-                resp.getWriter().print("Url enregistré : " + path + " -> " + methodName);
+                Object controller = invoker.getControllerClass().getDeclaredConstructor().newInstance();
+                Object result = invoker.getMethod().invoke(controller);
+
+                if (result instanceof String) {
+                    resp.setContentType("text/plain;charset=UTF-8");
+                    resp.getWriter().println(result);
+                    return;
+                }
+
+                if (result instanceof ModelView) {
+                    ModelView mv = (ModelView) result;
+                    String view = mv.getView();
+
+                    // Exemple : "test/pages/hello.jsp"
+                    String viewPath = "/" + view; // chemin relatif au contexte
+
+                    String realPath = context.getRealPath(viewPath);
+                    System.out.println("Vue recherchée : " + realPath);
+
+                    File jspFile = new File(realPath);
+                    if (jspFile.exists() && jspFile.isFile()) {
+                        // Transfert de la requête à Tomcat (servlet par défaut ou JSP compiler)
+                        RequestDispatcher rd = context.getRequestDispatcher(viewPath);
+                        rd.forward(req, resp);
+                    } else {
+                        resp.sendError(HttpServletResponse.SC_NOT_FOUND,
+                                "Vue introuvable : " + realPath);
+                    }
+                    return;
+                }
+
             } catch (Exception e) {
                 e.printStackTrace(resp.getWriter());
             }
