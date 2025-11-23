9fb170546cfb78d7f65f38b627a73925bd0499fd
MichaelETU003327 <mikermb18@gmail.com>
2025-11-23 20:52:44 +0300

    #6ter
    @ -0,0 +1,4 @@
    Sprint 6-ter :
    Atao afaka mirécupère an'ilay {valeur} ao anatin'ilay URL ilay framework dia lasa ireny (raha misy) no injecter-na ao amin'ilay méthode endpoint eo amin'ny placen'ilay argument mitovy anarana aminy
    Ohatra hoe fonctionTest(int id) izany mitady {id} ao amle URL dia izay valeur hitany ao no ampiasainy
    Dia mba hanamoraina ny fiainana amty ray ty ozy Mr Naina hoe asio ordre de priorité fotsiny ohatra hoe raha tss anle variable anaty url dia tadiavo anaty request ilay variable sinon soloy null na mithrow-eva Exception (exemple ana ordre io anah io fa izay tianareo)

---
+++ 6ter.md
@@ -0,0 +1,4 @@
+Sprint 6-ter : 
+Atao afaka mirécupère an'ilay {valeur} ao anatin'ilay URL ilay framework dia lasa ireny (raha misy) no injecter-na ao amin'ilay méthode endpoint eo amin'ny placen'ilay argument mitovy anarana aminy
+Ohatra hoe fonctionTest(int id) izany mitady {id} ao amle URL dia izay valeur hitany ao no ampiasainy
+Dia mba hanamoraina ny fiainana amty ray ty ozy Mr Naina hoe asio ordre de priorité fotsiny ohatra hoe raha tss anle variable anaty url dia tadiavo anaty request ilay variable sinon soloy null na mithrow-eva Exception (exemple ana ordre io anah io fa izay tianareo)
--- src/main/java/servlet/utils/UrlRouter.java
+++ src/main/java/servlet/utils/UrlRouter.java
@@ -4,15 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UrlRouter extends HashMap<String, MethodInvoker> {
-    public MethodInvoker findByUrl(String urlSpec) {
+
+    public RouteMatch findByUrl(String urlSpec) {
 
         for (Map.Entry<String, MethodInvoker> entry : this.entrySet()) {
             String urlGen = entry.getKey();
             MethodInvoker value = entry.getValue();
-            if (UrlMatcher.match(urlGen, urlSpec)) {
-                return value;
+
+            Map<String, String> diff = UrlMatcher.extractParams(urlGen, urlSpec);
+            if (diff != null) {
+                return new RouteMatch(value, diff);
             }
         }
         return null;
     }
 }
+
+
